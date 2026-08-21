370cec69294a40987174723f0dfa3af0d3590290
eltitanb <lorenzo@gattaca.com>
2026-08-21 10:55:05 +0100

    tokio

--- crates/types/src/runtime.rs
+++ crates/types/src/runtime.rs
@@ -5,8 +5,6 @@ static BACKGROUND_RT: OnceLock<tokio::runtime::Runtime> = OnceLock::new();
 pub fn init_background_runtime() {
     BACKGROUND_RT.get_or_init(|| {
         tokio::runtime::Builder::new_multi_thread()
-            .worker_threads(1)
-            .max_blocking_threads(1)
             .enable_all()
             .thread_name("bg-tokio")
             .build()
